b3f00ff1432cd7f1f9484881fc30c2fc9f7131c9
lea zaoui <lea.myriam.zaoui@gmail.com>
2020-05-27 16:17:32 +0200

    Simple registration process

--- controllers/controller.php
+++ controllers/controller.php
@@ -5,7 +5,28 @@ $action = $_GET["action"] ?? "display"; //si rien ne sort comme action, on execu
 switch ($action) {
 
   case 'register':
-    // code...
+    include "../models/UserManager.php";
+    if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['passwordRetype'])) {
+      $errorMsg = NULL;
+      if (!IsNicknameFree($_POST['username'])) {
+        $errorMsg = "Nickname already used.";
+      } else if ($_POST['password'] != $_POST['passwordRetype']) {
+        $errorMsg = "Passwords are not the same.";
+      } else if (strlen(trim($_POST['password'])) < 8) {
+        $errorMsg = "Your password should have at least 8 characters.";
+      } else if (strlen(trim($_POST['username'])) < 4) {
+        $errorMsg = "Your nickame should have at least 4 characters.";
+      }
+      if ($errorMsg) {
+        include "../views/RegisterForm.php";
+      } else {
+        $userId = CreateNewUser($_POST['username'], $_POST['password']);
+        $_SESSION['userId'] = $userId;
+        header('Location: ?action=display');
+      }
+    } else {
+      include "../views/RegisterForm.php";
+    }
     break;
 
   case 'logout':
--- models/UserManager.php
+++ models/UserManager.php
@@ -58,3 +58,28 @@ function GetUserIdFromUserAndPassword($username, $password)
 //   $oneUser = $users[0];
 //   return $oneUser['id'];
 // }
+
+function IsNicknameFree($nickname)
+{
+  global $PDO;
+  $response = $PDO->prepare("SELECT * FROM user WHERE nickname = :nickname ");
+  $response->execute(
+    array(
+      "nickname" => $nickname
+    )
+  );
+  return $response->rowCount() == 0;
+}
+
+function CreateNewUser($nickname, $password)
+{
+  global $PDO;
+  $response = $PDO->prepare("INSERT INTO user (nickname, password) values (:nickname , :password )");
+  $response->execute(
+    array(
+      "nickname" => $nickname,
+      "password" => $password
+    )
+  );
+  return $PDO->lastInsertId();
+}
